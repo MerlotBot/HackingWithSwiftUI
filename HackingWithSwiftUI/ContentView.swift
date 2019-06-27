58c4ca3b315edef656497bc9b053f657a4d58578
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-27 14:55:37 +0200

    Format text inside views

--- HackingWithSwiftUI/ContentView.swift
+++ HackingWithSwiftUI/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView : View {
+   
+    static let taskDateFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }()
+    
+    var dueDate = Date()
+    
+    
     var body: some View {
-        Text("Hello World")
+        
+        VStack {
+            Image("example-image")
+            
+            Text("Task due date: \(dueDate, formatter: Self.taskDateFormat)")
+                .background(Color.black)
+                .foregroundColor(Color.orange)
+                .font(.largeTitle)
+                .lineLimit(nil)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
